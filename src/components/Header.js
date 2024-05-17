ee528c8299f9048f1c744c0247193226d4836936
Selim Reja <48088144+SelimRejabd@users.noreply.github.com>
2024-05-17 21:08:49 +0600

    LOGIN changed as LOGOUT when user loged in.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,39 +1,54 @@
-import React from "react";
-import { Navbar, Nav, Container,} from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
+// src/components/Header.js
+
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout } from '../features/slice/UserLoginSlice';
+import { useNavigate } from 'react-router-dom';
 
 function Header() {
-  return (
-    <header>
-      <Navbar
-        expand="lg"
-        variant="dark"
-        collapseOnSelect
-        className="bg-dark text-white"
-      >
-        <Container>
-          <LinkContainer to="/">
-            <Navbar.Brand>TopShop</Navbar.Brand>
-          </LinkContainer>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <LinkContainer to="/cart">
-              <Nav.Link>
-                <i className="fas fa-shopping-cart"></i>Cart
-              </Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/login">
-              <Nav.Link>
-                <i className="fas fa-user"></i>Login
-              </Nav.Link>
-              </LinkContainer>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </header>
-  );
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const user = useSelector((state) => state.user.user);
+
+    const handleLogout = () => {
+        dispatch(logout());
+        navigate('/login');
+    };
+
+    return (
+        <header>
+            <Navbar expand="lg" variant="dark" collapseOnSelect className="bg-dark text-white">
+                <Container>
+                    <LinkContainer to="/">
+                        <Navbar.Brand>TopShop</Navbar.Brand>
+                    </LinkContainer>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <LinkContainer to="/cart">
+                                <Nav.Link>
+                                    <i className="fas fa-shopping-cart"></i>Cart
+                                </Nav.Link>
+                            </LinkContainer>
+                            {user ? (
+                                <Nav.Link onClick={handleLogout}>
+                                    <i className="fas fa-user"></i>Logout
+                                </Nav.Link>
+                            ) : (
+                                <LinkContainer to="/login">
+                                    <Nav.Link>
+                                        <i className="fas fa-user"></i>Login
+                                    </Nav.Link>
+                                </LinkContainer>
+                            )}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    );
 }
 
 export default Header;
